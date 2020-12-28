faf76133fd585527705dea967a5de8b6fafdeb4a
Restyled.io <commits@restyled.io>
2020-12-28 06:04:58 +0000

    Restyled by clang-format

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const VideoController = function (isHovering, videoElement) {
-    if (isHovering == true) {
-        videoElement.play();
-    } else if (isHovering == false) {
-        videoElement.pause();
-    }
+const VideoController = function(isHovering, videoElement) {
+  if (isHovering == true) {
+    videoElement.play();
+  } else if (isHovering == false) {
+    videoElement.pause();
+  }
 };
